758aa6c5e4525e160fee0b096b1e2e5e043511d2
Manel <a18manfermar@inspedralbes.cat>
2020-10-20 18:32:57 +0200

    Script finalizado

--- script.sh
+++ script.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
 
 # 1-Make sure the script is being executed with superuser privileges.
+if [ $EUID -ne 0 ]
+then
+	echo "Se neceista permisos de administrador."
+       	echo "Use sudo ./script.sh"
+else
+	# 2-Get the username (login)
+	read -p "Introduce nombre de usuario: " username
 
-# 2-Get the username (login).
+	# 3-Get the real name (contents for the description field)
+	read -p "Introduce nombre completo del usuario: " descripcion	
 
-# 3-Get the real name (contents for the description field).
+	# 4-Get the password
+	read -p "Introduce Contraseña: " password
 
-# 4-Get the password.
+	# 5-Create the user.
+	useradd -c $descripcion -m $username
 
-# 5-Create the user.
+	# 6-Check to see if the useradd command succeeded.
+	if [[ "${?}" -ne 0 ]]
+	then
+		echo "No se ha podido crear el usuario"
+		exit 1
+	fi
 
-# 6-Check to see if the useradd command succeeded.
+	# 7-Set the password.
+	echo -e "$password\n$password" |passwd $username
+	passwd -e $username
 
-# 7-Set the password.
+	# 8-Check to see if the passwd command succeeded.
+	if [[ $? -ne 0 ]]
+	then
+		echo "La contraseña no se ha podido añadir"
+		exit 1
+	fi
 
-# 8-Check to see if the passwd command succeeded.
 
-# 9-Force password change on first login.
+	# 9-Force password change on first login.
+	passwd -e ${username}
+	
+	# 10-Display the username, password, and the host where the user was create
+	echo
+	echo "username: $username"
+	echo "password: $password"
+	echo "host: $HOSTNAME"
+	exit 0
 
-# 10-Display the username, password, and the host where the user was create
+fi
