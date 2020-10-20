a11b4f1dbcdf537329d17886420dde6ff21b9425
Manel <a18manfermar@inspedralbes.cat>
2020-10-20 17:22:41 +0200

    creacion del arcihvo script

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 1-Make sure the script is being executed with superuser privileges.
+
+# 2-Get the username (login).
+
+# 3-Get the real name (contents for the description field).
+
+# 4-Get the password.
+
+# 5-Create the user.
+
+# 6-Check to see if the useradd command succeeded.
+
+# 7-Set the password.
+
+# 8-Check to see if the passwd command succeeded.
+
+# 9-Force password change on first login.
+
+# 10-Display the username, password, and the host where the user was create
